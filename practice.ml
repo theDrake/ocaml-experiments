af657c263e5818bf9bc6a44fa404c9dff72fd2ec
David C. Drake <dcdrake42@gmail.com>
2014-10-06 15:19:43 -0600

    Add "practice.ml"

---
+++ practice.ml
@@ -0,0 +1,14 @@
+(******************************************************************************
+   Filename: practice.ml
+
+     Author: David C. Drake (http://davidcdrake.com)
+
+Description: A practice file for experimenting with the OCaml programming
+             language.
+******************************************************************************)
+
+let average a b =
+  (a +. b) /. 2.0
+
+prompt_string ("Name: ")
+
